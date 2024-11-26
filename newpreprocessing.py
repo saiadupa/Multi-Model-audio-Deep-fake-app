881e34944c6ffd8e8b722132b4973885c8706ca4
NITHIN SAI ADUPA <96478917+saiadupa@users.noreply.github.com>
2024-11-25 21:04:45 -0500

    Update newpreprocessing.py

--- newpreprocessing.py
+++ newpreprocessing.py
@@ -16,15 +16,20 @@ def trim_silence(audio, silence_thresh=-40, min_silence_len=500, buffer=500):
     trimmed_audio = audio[start_trim:end_trim]
     return trimmed_audio
 
+def repeat_audio(audio, target_duration_ms):
+    while len(audio) < target_duration_ms:
+        audio += audio
+    return audio[:target_duration_ms]
+
 def preprocess_audio(audio_file, speaker_name='unknown', chunk_duration=12, format=None, save_chunks=False, out_dir='./chunks/'):
     if save_chunks:
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
         elif len(os.listdir(out_dir)) != 0:
             files = os.listdir(out_dir)
-            print(files)
             for f in files:
                 os.remove(os.path.join(out_dir, f))
+
     if format is None:
         _, f_ext = os.path.splitext(audio_file)
         format = f_ext[1:].lower()
@@ -34,11 +39,12 @@ def preprocess_audio(audio_file, speaker_name='unknown', chunk_duration=12, form
     if audio is None:
         return {}
 
-    while len(audio) < 12000: 
-        audio += audio
-
     total_duration = len(audio)
-
+    
+    # Ensure the audio length is at least as long as the chunk duration
+    target_duration_ms = chunk_duration * 1000  # 12 seconds in milliseconds
+    audio = repeat_audio(audio, target_duration_ms)
+    
     base_index = 0
     chunk_index = 1
     start_time = 0
@@ -46,27 +52,26 @@ def preprocess_audio(audio_file, speaker_name='unknown', chunk_duration=12, form
     audio_dict = {}
 
     while start_time < total_duration:
-        end_time = start_time + chunk_duration * 1000
-        
-        file_index = base_index + chunk_index
-        out_filename = speaker_name + f"_{file_index}"
-
+        end_time = start_time + target_duration_ms
         chunk = audio[start_time:end_time]
+
+        chunk = repeat_audio(chunk, target_duration_ms)
 
         if not (is_silent(chunk) or len(chunk) < 5000):
             if save_chunks:
-                output_file = os.path.join(out_dir, out_filename + '.flac')
+                out_filename = f"{speaker_name}_{base_index + chunk_index}.flac"
+                output_file = os.path.join(out_dir, out_filename)
                 chunk.export(output_file, format="flac")
             
             chunk_np = np.array(chunk.get_array_of_samples(), dtype=np.float32)
             audio_dict[out_filename] = chunk_np
 
-        start_time = start_time + chunk_duration * 1000
+        start_time += target_duration_ms
         chunk_index += 1
 
     return audio_dict
 
 def is_silent(audio_segment):
     rms = audio_segment.rms
-    silence_threshold = 0 
+    silence_threshold = 0  
     return rms < silence_threshold
